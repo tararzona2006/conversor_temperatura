f218b42038ddb05209f66662889ee23503e0447f
tarazona2006 <andressuarez@colegioguanenta.edu.co>
2023-02-16 15:32:38 -0500

    se creo el diagrama de flujo y se agrego al README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # conversor_temperatura
-programa para convertir una cantidad dada de grados centigrados a su equivalente en Farenheit y Kelvin
+# programa para convertir una cantidad dada de grados centigrados a su equivalente en Farenheit y Kelvin
+# diseño
+## diagrama de flujo
+![diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ conversor_temperatura.py
@@ -0,0 +1,19 @@
+# programa para realizar los operadores, usando las diferentes funciones
+
+print("------------------------------------------")
+print("---el resultado de las operaciones son:---")
+print("------------------------------------------")
+
+# input
+c = int(input("digite el valor de c:"))
+
+# processing
+k = c + 273.15
+f = c * 1.8 + 32
+
+# output
+print("el resultado grados kelvin " + str(k))")
+print("el resultado grados fahrenheit " + str(f))")((
+print("--------------------")
+print("-----resultados-----")
+print("--------------------")
